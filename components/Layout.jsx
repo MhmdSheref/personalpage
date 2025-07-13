636228d147d9a187dcdf600c42968fba33414824
MhmdSheref <88167888+MhmdSheref@users.noreply.github.com>
2025-07-13 16:06:51 +0300

    analytics

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import { Analytics } from '@vercel/analytics/next';
 export default function Layout({children}) {
     return (
         <>
             <Header/>
             <div className="AllContent">{children}</div>
+            <Analytics />
         </>
     )
 
